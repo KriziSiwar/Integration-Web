33875ea59762c297a5d3c693f70e2a8cf480f02a
CHAMKHI Chiheb <chiheb.chamkhi@esprit.tn>
2025-03-01 14:30:10 +0100

    Chatbot+support

---
+++ tailwind.config.js
@@ -0,0 +1,22 @@
+module.exports = {
+  content: ["./**/*.{html,js}"],
+  theme: {
+    extend: {
+      
+      colors: {
+        primary: "#314584",
+        secondary: "#ff7946",
+        tertiary: "#6e757a",
+      },
+      fontFamily: {
+        body: ["Work Sans"],
+      },
+      fontSize: {
+        navSize: "24px",
+        heroSize: "74px",
+        pSize: "18px",
+      },
+    },
+  },
+  plugins: [],
+}
